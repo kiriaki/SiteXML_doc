8c4ebb332e6559c4ae6cee828ff7583319cdbbfd
Kiriaki Konstantinidou <kiriaki@itsak.gr>
2025-11-22 19:35:55 +0200

    Update conf.py for SiteXML

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SiteXML'
+copyright = '2025, ORFEUS'
+author = 'Kyriaki Konstantinidou (kiriaki@itsak.gr)'
 
 release = '0.1'
 version = '0.1.0'
