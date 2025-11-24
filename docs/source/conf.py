0500dc934c17929ec078200486d3575660dd172a
Kiriaki Konstantinidou <kiriaki@itsak.gr>
2025-11-24 18:21:43 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,12 +6,28 @@ project = 'SiteXML'
 copyright = '2025, ORFEUS'
 author = 'Kyriaki Konstantinidou (kiriaki@itsak.gr)'
 
-release = '0.1'
-version = '0.1.0'
+version = '1.3'
 
-# -- General configuration
+# Documentation version, schema + date
+# ALSO UPDATE the release documentation version in overview.rst
+doc_version = version + ' (2025-11-25)'
 
+# Allow |doc_version| to be used in RST
+rst_epilog = '.. |doc_version| replace:: %s' % doc_version
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+spelling_word_list_filename=['spelling/text_words.txt',
+                             'spelling/schema_words.txt',
+                             ]
 extensions = [
+    'sphinxcontrib.contentui',
+    'linuxdoc.rstFlatTable',      # Implementation of the 'flat-table' reST-directive
+    #'sphinxcontrib.spelling',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
@@ -25,11 +41,47 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+# Note, some of these, like level-*, are included from other .rst files
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','examples']
+                    
 # -- Options for HTML output
+# -- Options for HTML output -------------------------------------------------
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 0,
+    'sticky_navigation': False,
+}
+html_logo='_static/FDSN-logo.png'
+html_favicon = '_static/favicon.ico'
+html_title = 'ORFEUS SiteXML'
+html_show_sphinx = False
+html_search_language = 'en'
+
+navigation_depth = -2
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_css_files = [
+  'css/custom.css',
+  'css/theme_overrides.css',
+]
+
+html_js_files = [
+  'js/sidebar_context.js'
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
